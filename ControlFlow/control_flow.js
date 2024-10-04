912382d949adb168a0760cf6b455498258b2de2e
ChristosDi <christosdimitriou.swe@gmail.com>
2024-10-04 17:59:04 -0400

    Just a message

--- ControlFlow/control_flow.js
+++ ControlFlow/control_flow.js
@@ -2,8 +2,6 @@ let userRole = "admin";
 let accessLevel;
 let isLoggedIn = true;
 let userMessage;
-let userType="subscriber";
-let userCategory;
 
 if (isLoggedIn){
     if (userRole === "admin"){
@@ -20,20 +18,30 @@ if (isLoggedIn){
     userMessage = "Please log in to access the system.";
 }
 
+let userType="subscriber";
+let userCategory;
+let isAuthenicated = true;
+let authenticationStatus;
+
 switch (userType){
     case "admin":
+        authenticationStatus = isAuthenicated ? "Authenticated" : "Not authenticated";
         userCategory = "Administrator";
         break;
     case "manager":
+        authenticationStatus = isAuthenicated ? "Authenticated" : "Not authenticated";
         userCategory = "Manager";
         break;
     case "subscriber":
+        authenticationStatus = isAuthenicated ? "Authenticated" : "Not authenticated";
         userCategory = "Subscriber";
         break;
     default:
         userCategory = "Unknown";
 }
 
+
 console.log("Access Level:", accessLevel);
 console.log("User Message:", userMessage);
 console.log("User Category:", userCategory);
+console.log("Authentication Status:", authenticationStatus);
