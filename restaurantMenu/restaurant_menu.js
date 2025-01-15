45785ca42e39efc4fefe984d7dcf59b3db1c908f
ChristosDi <christosdimitriou.swe@gmail.com>
2025-01-15 15:01:21 -0500

    all

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,6 @@
+const breakfastMenu = ['Panckaes','Eggs Benedict', 'Oatmeal', 'Frittata'];
+const maincourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML  = breakfastMenuItemsHTML;
