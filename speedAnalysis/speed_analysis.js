0ea38ee244ac55f97532be9a90a7b81d43f84fd2
ChristosDi <christosdimitriou.swe@gmail.com>
2025-01-20 17:10:33 +0000

    finished

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -20,4 +20,23 @@ function endTest(){
     var timeElapsed = (endTime-startTime)/1000;
     var userTypedText = document.getElementById("userInput").value;
 
+    var typedWords = userTypedText.split(/\s+/).filter(function(word){
+        return word !==""
+    }).length;
+
+    var wpm = 0; //Default Value
+
+    if (timeElapsed !==0 && !isNaN(typedWords)){
+        wpm= Math.round((typedWords/timeElapsed)*60) ;
+    }
+
+    //Display results
+    var outputDiv= document.getElementById("output");
+    outputDiv.innerHTML= "<h2>Typing Test Results:</h2>"+"Total Lenght:"+ userTypedText.length +"<p>Words Typed:"+ typedWords+"</p>"+"<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
+                "<p>Words Per Minute (WPM): " + wpm + "</p>";
+
+    //Reset button
+    var button = document.getElementById("btn");
+    button.innerHTML = "Start Test";
+    button.onclick = startTest;
 };
